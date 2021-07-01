49ad57bc50db7e67fee04fba5fa0876139dd9bdd
Kostad1n <71552347+KostadinKim@users.noreply.github.com>
2021-07-01 13:57:05 +0600

    Add files via upload

---
+++ TSIS2/leet_4.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def largestAltitude(self, gain):
+        highest = 0
+        altitude = 0
+        for i in gain:
+            altitude += i
+            if altitude > highest:
+                highest = altitude
+        return highest
